35ea0af6adf20fac866fdceab6d6c473402f5cb7
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2023-10-30 20:07:52 -0400

    its ok now but like

--- src/routes/(init).tsx
+++ src/routes/(init).tsx
@@ -1,6 +1,7 @@
-import { useNavigate } from 'solid-start'
+import { redirect, useNavigate } from 'solid-start'
 export default function init() { 
   const nav = useNavigate()
+  nav('/app/home')
 return (
 
 <div />
--- src/routes/app/register/index.tsx
+++ src/routes/app/register/index.tsx
@@ -1,100 +1,89 @@
-import  { trpc } from '~/utils/trpc';
 import {
   POST, GET
 } from '~/routes/api/register';
-import { redirect, useRequest } from 'solid-start/server'
-import { setCookie } from 'undici';
-import { createCookie } from 'solid-start';
-import { createServerData$,createServerAction$ } from 'solid-start/server';
-import { createRouteAction } from 'solid-start';
-import { createCookieSessionStorage } from 'solid-start';
-import { createUserSession,getUser } from '~/lib/session';
+import { createServerData$, createServerAction$ } from 'solid-start/server';
+import { createUserSession, getUser } from '~/lib/session';
+import z from 'zod';
+import { ZodType } from "~/utils/schemas"
+import { catchError } from 'solid-js';
+
 // 
+const { formSchema } = ZodType()
 export default function Register() {
-    const redir = async () => {
-        createUserSession ( "hello" , "/app/profile" )
-    }
-    const [Form, sendForm] = createServerAction$(() => createUserSession ( "hello" , "/app/profiles" ))
+  const redir = async () => {
+    createUserSession("hello", "/app/profile")
+  }
 
- 
-const hello = "hello"  
-
-  
-
-
-
-
-
-
-  
- //  send data to the server the form data  
+  type registerForm = z.infer<typeof formSchema> | z.ZodIssue[]
+  const [Form, sendForm] = createServerAction$(() => createUserSession("hello", "/app/profiles"))
+  //  send data to the server the form data  
   const [form, setForm] = createServerAction$(async (form: FormData, { request }) => {
-
-    if (!form.get("field_one")) {
-       console.log("hello from server")
-      return  { error: "field_one is required"}
-    } 
-     console.log(form.get("field_one"))
-    
-   return (createUserSession ( "hello" , "/app/profiles" ))
+    const data = formSchema.parse(form)
+    return data.email
   })
-  
+  const Validation = (formData: FormData) => {
+    type registerForm = z.infer<typeof formSchema> | z.ZodIssue[]
+    const result = formSchema.safeParse(formData);
+    return result.success ? result.data : result.error.issues.map((issue) => issue)
+  }
 
-const [data, setData] = createServerAction$((_,e) => {  
-  e.request.formData().then((data) => {
-     console.log(data.get("field_one"))
-  })
-    return  getUser(e.request)  
-  } )
-  
+  const onClientSubmit = (data: registerForm | z.ZodIssue[]) => {
+    if (Array.isArray(data)) {
+
+      console.log("todo show error")
+      return false
+    }
+    else {
+      return true
+
+    }
+  }
+  const onServerSubmit = (data: FormData) => {
+    console.log(data.get("email"))
+    try {
+      setForm(data).then((servData) => {
+        if (typeof servData == typeof "string") {
+          console.log(servData)
+        }
+      })
+    }
+    catch (e) {
+      console.log(e)
+    }
+
+  }
   const handleSubmit = (e: Event) => {
     const form = e.target as HTMLFormElement;
-    // get form data 
-    const formData = new FormData(form);
-    // this will send data to the server 
-     console.log(formData.get("field_one"))
-     setForm(formData).then((data) => {
-       if (typeof data === "string") {
-         console.log(data)
-       }
-        console.log("hello")
-      })
-    //form data 
-    
     e.preventDefault();
-    setData().then(() => {
-
-      console.log(data.result?.data)
-      /*
-          red().then ( () => {
-              console.log("hello")
-          } )
-        */
-      // check the cookie is set
-          })
-} 
+    const formData = new FormData(form);
+    const validatedContent = Validation(formData)
+    onClientSubmit(validatedContent) ? onServerSubmit(formData) : console.log("fall back to error state here")
+  }
 
   return (
-       <div class=" flex flex-col items-center justify-center 
+    <div class=" flex flex-col items-center justify-center 
        border border-1 border-gray-900 rounded-lg  my-[100px] p-[2em]"  >
       <h1> Register to this shit fest </h1>
-               <form onSubmit={(e) => handleSubmit(e)}> 
-    <div class={"flex flex-col items-center justify-center    w-96 h-[25rem] border-1  "}>
-           
-      <div class="flex flex-col items-center justify-center    w-96 h-[25rem] border-1  ">
-      <input name="field_one" type="text"  placeholder="username" class="bg-transparent border border-1 border-gray-900 text-center text-slate-50" />
-      <input name="field_two" type="text" placeholder="password" class="bg-transparent border border-1 border-gray-900 text-slate-50 text-center" />
-      <input name="field_three" type="text" placeholder="confirm password" class="bg-transparent border border-1 border-gray-900text-slate-50  text-center" />
-             
-        <button class="mt-6" type="submit" > hello </button>
-         </div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <div class={"flex flex-col items-center justify-center    w-96 h-[25rem] border-1  "}>
+
+          <div class="flex flex-col items-center justify-center    w-96 h-[25rem] border-1  ">
+            <input name="email" type="text" placeholder="email" class="bg-transparent border border-1 border-gray-900 text-center text-slate-50" />
+
+            <input name="username" type="text" placeholder="username" class="bg-transparent border border-1 border-gray-900 text-center text-slate-50" />
+
+            <input name="password" type="text" placeholder="password" class="bg-transparent border border-1 border-gray-900 text-slate-50 text-center" />
+            <input name="re_password" type="text" placeholder="confirm password" class="bg-transparent border border-1 border-gray-900text-slate-50  text-center" />
+
+            <button class="mt-6" type="submit" > hello </button>
+          </div>
 
 
-      </div>
+        </div>
 
       </form>
 
-</div>
+    </div>
   );
 }
 
