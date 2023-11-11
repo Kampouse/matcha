8d4601b5490bd442a5a6ffbb6056a510188e6700
jean-philippe martel <j-p@jean-philippes-MacBook-Air.local>
2023-11-11 17:45:09 -0500

    who need that

--- src/server/trpc/database.ts
+++ src/server/trpc/database.ts
@@ -6,9 +6,5 @@ const config = {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD
 }
-const configSchema = z.object({
-    host: z.string(),
-    username: z.string(),
-    password: z.string(),
-})
+
 export const Clientdb = new Client(config).connection()
